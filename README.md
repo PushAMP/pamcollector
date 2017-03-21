06cad08961e2a91b76235554c3d88faedcd59166
Dmitriy Zhiltsov <dmitry@pushamp.com>
2017-03-21 19:07:12 +0300

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pamcollector
 PushAmp Metric Collector
+
+Input proto
+===========
+
+ts: f64
+app_name: String
+app_layer: String
+value: f32
+
+Example 
+    
+    $ nc -u 127.0.0.1 12345
+    {"ts": 1490170982, "value": 0.4332, "app_name": "App1", "app_layer": "DB"}
--- src/pamcollector/output.rs
+++ src/pamcollector/output.rs
@@ -14,9 +14,21 @@ impl ConsoleOutput {
     }
 }
 
+fn output_spawn(bytes: &Vec<u8>, res_vec: &mut Vec<String>) {
+    let out = String::from_utf8_lossy(&bytes);
+    let decoded = format!("{}", out);
+    res_vec.push(decoded);
+    if res_vec.len() > 2 {
+        println!("FULL");
+        println!("{:?}", res_vec);
+        res_vec.clear();
+    }
+}
+
+
 impl Output for ConsoleOutput {
     fn start(&self, arx: Arc<Mutex<Receiver<Vec<u8>>>>) {
-        let mut res_vec = Vec::new();
+        let mut res_vec: Vec<String> = Vec::new();
         thread::spawn(move || loop {
             let bytes = match {
                 arx.lock().unwrap().recv()
@@ -24,14 +36,7 @@ impl Output for ConsoleOutput {
                 Ok(line) => line,
                 Err(_) => return,
             };
-            let out = String::from_utf8_lossy(&bytes);
-            let decoded = format!("{}", out);
-            res_vec.push(decoded);
-            if res_vec.len() > 2 {
-                println!("FULL");
-                println!("{:?}", res_vec);
-                res_vec.clear();
-            }
+            output_spawn(&bytes, &mut res_vec)
         });
     }
 }
