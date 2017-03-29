4513d509f32745ae66694fba32b9791c7ed89b49
Dmitriy Zhiltsov <dmitry@pushamp.com>
2017-03-29 18:42:34 +0300

    Add labels support

--- src/pamcollector/metric.rs
+++ src/pamcollector/metric.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use chrono::prelude::NaiveDateTime;
 
 #[derive(Debug, Serialize, Deserialize, Default)]
@@ -8,10 +9,25 @@ pub struct Metric {
     value: f32,
     operation: Option<String>,
     metric_name: String,
+    labels: Option<HashMap<String, String>>,
 }
 
 impl Metric {
     pub fn to_val(&self) -> Vec<String> {
+        let (labels, labels_val) = match self.labels {
+            Some(ref v) => {
+                let mut vec1 = Vec::with_capacity(v.len());
+                let mut vec2 = Vec::with_capacity(v.len());
+                for (k, v) in v.iter() {
+                    vec1.push(k);
+                    vec2.push(v);
+                }
+                println!("{:?} {:?}", vec1, vec2);
+                (vec1, vec2)
+            }
+            None => (Vec::new(), Vec::new()),
+        };
+        println!("{:?} {:?}", labels, labels_val);
         vec![format!("'{}'", self.metric_name),
              format!("{}", self.value),
              format!("'{}'", NaiveDateTime::from_timestamp(self.ts as i64, 0)),
@@ -20,3 +36,4 @@ impl Metric {
              format!("'{}'", self.operation.as_ref().unwrap_or(&String::new()))]
     }
 }
+
