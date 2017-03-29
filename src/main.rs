dfcfeb7b0c54158c57a14f0939b948b26daf286d
Dmitriy Zhiltsov <dmitry@pushamp.com>
2017-03-29 15:53:56 +0300

    Add stdin params

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,24 @@ extern crate serde_json;
 extern crate hyper;
 extern crate chrono;
 extern crate toml;
-
 #[macro_use]
 extern crate serde_derive;
 mod pamcollector;
 
 use std::io::{stderr, Write};
-use clap::App;
+use clap::{App, Arg};
 
 const VERSION: &'static str = "0.0.1";
 const DEFAULT_CONF_FILE: &'static str = "pamcollector.toml";
 
 
 fn main() {
-    let _ = App::new("PaMCollector")
+    let matches = App::new("PaMCollector")
         .version(VERSION)
         .about("PushAMP Metric Collector")
+        .arg(Arg::with_name("config_file").short("c").long("config").help("Configuration file").value_name("FILE"))
         .get_matches();
     let _ = writeln!(stderr(), "PaMCollector {}", VERSION);
-    let config_path = DEFAULT_CONF_FILE;
+    let config_path = matches.value_of("config_file").unwrap_or(DEFAULT_CONF_FILE);;
     pamcollector::start(&config_path)
 }
